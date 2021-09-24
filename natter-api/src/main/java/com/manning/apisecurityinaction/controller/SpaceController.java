cbfa7777c09d8b231a5e2516c86d3e9a59dad4d9
fatima.azzam <fatima.azzam@i-jaffa.net>
2021-09-24 13:31:09 +0300

    Test push

--- natter-api/src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
+++ natter-api/src/main/java/com/manning/apisecurityinaction/controller/SpaceController.java
@@ -23,6 +23,7 @@ public class SpaceController {
     if (spaceName.length() > 255) {
       throw new IllegalArgumentException("space name too long");
     }
+    //No check for input type for both user & message
     var owner = json.getString("owner");
     if (!owner.matches("[a-zA-Z][a-zA-Z0-9]{1,29}")) {
       throw new IllegalArgumentException("invalid username");
